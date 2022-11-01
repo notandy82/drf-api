dbd9bf215f7c066ac2b46c45ec30be9a2623dddb
notandy82 <notandystanek@gmail.com>
2022-11-01 19:23:58 +0000

    Add profile filter

--- profiles/views.py
+++ profiles/views.py
@@ -47,6 +47,7 @@
 
 from django.db.models import Count
 from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
@@ -64,7 +65,11 @@ class ProfileList(generics.ListAPIView):
     ).order_by('-created_at')
     serializer_class = ProfileSerializer
     filter_backends = [
-        filters.OrderingFilter
+        filters.OrderingFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'owner__following__followed__profile',
     ]
     ordering_fields = [
         'posts_count',
